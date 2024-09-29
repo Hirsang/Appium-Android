c5e6c8a9ebd59ca240a3e597a415e029bb69dc01
hirsang whisnu <hirsangwhisnu@gmail.com>
2024-09-29 12:51:52 +0700

    penambahan filter harga dan share product

--- pageobjects/filterproduct.js
+++ pageobjects/filterproduct.js
@@ -50,6 +50,104 @@ class FilterProduct{
         await this.driver.$('android=new UiSelector().resourceId("com.tokopedia.tkpd:id/buttonApplySortFilter")').click();
     }
 
+    async price(){
+        const startX = 536;
+        const startY = 1233;
+        const endY = 200; // Y akhir untuk scroll
+
+        // Loop scroll
+        const maxScrolls = 10; // Jumlah maksimum scroll
+        let scrollCount = 0;
+        let elementExists = false;
+
+        while (!elementExists && scrollCount < maxScrolls) {
+            // Scroll ke bawah
+            await this.driver.performActions([
+                { 
+                    type: 'pointer', 
+                    id: 'finger1', 
+                    parameters: { pointerType: 'touch' }, 
+                    actions: [
+                        { type: 'pointerMove', duration: 0, x: startX, y: startY },
+                        { type: 'pointerDown', duration: 0 },
+                        { type: 'pointerMove', duration: 500, x: startX, y: endY },
+                        { type: 'pointerUp', duration: 0 }
+                    ] 
+                }
+            ]);
+
+            // Cek apakah elemen ada setelah scroll
+            const minamount = await this.driver.$('android=new UiSelector().text("Terendah")');
+            elementExists = await minamount.isExisting()
+            const maxamount = await this.driver.$('android=new UiSelector().text("Tertinggi")');
+            elementExists = await maxamount.isExisting()
+
+            if (elementExists) {
+                await minamount.click();
+                await minamount.setValue(10000);
+
+                await maxamount.click();
+                await maxamount.setValue(50000);
+                console.log('Elemen ditemukan dan diklik.');
+            } else {
+                console.log(`Scroll ${scrollCount + 1} dilakukan, elemen belum ditemukan.`);
+            }
+
+            scrollCount++;
+        }
+        if (!elementExists) {
+            console.log('Elemen tidak ditemukan setelah maksimum scroll.');
+        }
+    }
+
+    async submitfilter(){
+        const startX = 536;
+        const startY = 1233;
+        const endY = 200; // Y akhir untuk scroll
+
+        // Loop scroll
+        const maxScrolls = 10; // Jumlah maksimum scroll
+        let scrollCount = 0;
+        let elementExists = false;
+
+        while (!elementExists && scrollCount < maxScrolls) {
+            // Scroll ke bawah
+            await this.driver.performActions([
+                { 
+                    type: 'pointer', 
+                    id: 'finger1', 
+                    parameters: { pointerType: 'touch' }, 
+                    actions: [
+                        { type: 'pointerMove', duration: 0, x: startX, y: startY },
+                        { type: 'pointerDown', duration: 0 },
+                        { type: 'pointerMove', duration: 500, x: startX, y: endY },
+                        { type: 'pointerUp', duration: 0 }
+                    ] 
+                }
+            ]);
+
+            // Cek apakah elemen ada setelah scroll
+            const targetElement = await this.driver.$('android=new UiSelector().resourceId("com.tokopedia.tkpd:id/buttonApplySortFilter")');
+            elementExists = await targetElement.isExisting();
+
+            if (elementExists) {
+                await targetElement.click();
+                console.log('Elemen ditemukan dan diklik.');
+            } else {
+                console.log(`Scroll ${scrollCount + 1} dilakukan, elemen belum ditemukan.`);
+            }
+
+            scrollCount++;
+        }
+        if (!elementExists) {
+            console.log('Elemen tidak ditemukan setelah maksimum scroll.');
+        }
+    }
+
+
+
+    
+
     
 
 }
